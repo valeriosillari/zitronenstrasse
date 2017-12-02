dd03306045c84e3839462e145ec3847426dd6095
Valerio Sillari <valerio.sillari@gmail.com>
2017-12-02 15:09:54 +0100

    [skip ci] set 3 basic pages with routing, on unique layout and nav bar element

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+  .container
+    section.section.section-home(style='background: green; color: white; min-height: 1200px;')
+      h1
+        | About
+</template>
---
+++ pages/contact.vue
@@ -0,0 +1,6 @@
+<template lang="pug">
+  .container
+    section.section.section-home(style='background: blue; color: white; min-height: 1200px;')
+      h1
+        | Contact
+</template>
