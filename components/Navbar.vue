0786e2bce2c86f1fa23539c122262f17ac8ecb69
Valerio Sillari <valerio.sillari@gmail.com>
2017-12-03 19:03:14 +0100

    [skip ci] set navi as dark.

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  nav.navbar.navbar-expand-lg.navbar-light.bg-light.fixed-top
+  nav.navbar.navbar-expand-lg.navbar-dark.bg-dark.fixed-top
 
     nuxt-link.navbar-brand(to='/')
       | Home Nav Bar
