129c4741de31710d6ddf87dfd201c88a8713c881
valerio sillari <valerio.sillari@gmail.com>
2017-12-12 00:19:35 +0100

    cleanup router. locally this solve the problem

--- plugins/vue_analytics.js
+++ plugins/vue_analytics.js
@@ -1,12 +1,7 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import VueAnalytics from 'vue-analytics'
-
-// TODO: check here, you should pass the routes. not sure if nuxt does it automatically...
-const router = new VueRouter()
 
 // TODO: set analytics keyID as ENV key?
 Vue.use(VueAnalytics, {
-  id: 'UA-110867603-1',
-  router
+  id: 'UA-110867603-1'
 })
