2f7b699cc0585f63bc56be06f56aa687a16fa67c
valerio sillari <valerio.sillari@gmail.com>
2017-12-14 00:03:44 +0100

    [skip ci] play with colors.

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -46,157 +46,134 @@
     +paddingBoxDefaut()
     background: $color_nav_bar_bg
 
-  // .btn-menu {
-  //   position: relative;
-  //   float: right;
-  //   height: $button_height;
-  //   width: $button_width;
-  //   cursor: pointer;
-  //   transition: opacity .25s ease;
-  //   .btn-menu-line {
-  //     background: $color_main;
-  //     border: none;
-  //     height: 5px;
-  //     width: 100%;
-  //     position: absolute;
-  //     top: 0;
-  //     left: 0;
-  //     transition:  all .35s ease;
-  //     cursor: pointer;
+  .btn-menu
+    position: relative
+    float: right
+    height: $button_height
+    width: $button_width
+    cursor: pointer
+    transition: opacity .25s ease
+    .btn-menu-line
+      background: $color_main
+      border: none
+      height: 5px
+      width: 100%
+      position: absolute
+      top: 0
+      left: 0
+      transition:  all .35s ease
+      cursor: pointer
+
+      &:nth-of-type(2)
+        top: 11px
       
-  //     &:nth-of-type(2) {
-  //       top: 11px;
-  //     }
+      &:nth-of-type(3)
+        top: 22px
+
+    &:hover
+      .btn-menu-line
+        background: $color_main_02
+
+  .overlay
+    position: fixed
+    background: $color_main
+    top: 0
+    left: 0
+    width: 100%
+    height: 0%
+    opacity: 0
+    visibility: hidden
+    transition: opacity .35s, visibility .35s, height .35s
+    overflow: hidden
+
+    nav
+      position: relative
+      height: 70%
+      top: 50%
+      transform: translateY(-50%)
+      font-size: 50px
+      text-align: center
+
+    ul
+      list-style: none
+      padding: 0
+      margin: 0 auto
+      display: inline-block
+      position: relative
+      height: 100%
       
-  //     &:nth-of-type(3) {
-  //       top: 22px;
-  //     }
-  //   }    
-  //   &:hover {
-  //     .btn-menu-line {
-  //       background: $color_main_02;
-  //     }
-  //   }
+    li
+      display: block
+      height: 30%
+      height: calc(100% / 3)
+      min-height: 50px
+      position: relative
+      opacity: 0
 
-  // }
-
-
-
-  // .overlay {
-  //   position: fixed;
-  //   background: $color_main;
-  //   top: 0;
-  //   left: 0;
-  //   width: 100%;
-  //   height: 0%;
-  //   opacity: 0;
-  //   visibility: hidden;
-  //   transition: opacity .35s, visibility .35s, height .35s;
-  //   overflow: hidden;
-
-  //   nav {
-  //     position: relative;
-  //     height: 70%;
-  //     top: 50%;
-  //     transform: translateY(-50%);
-  //     font-size: 50px;
-  //     text-align: center;
-  //   }
-  //   ul {
-  //     list-style: none;
-  //     padding: 0;
-  //     margin: 0 auto;
-  //     display: inline-block;
-  //     position: relative;
-  //     height: 100%; 
+    a
+      display: block
+      position: relative
+      color: $color_link
+      text-decoration: none
+      overflow: hidden
+      &:hover:after,
+      &:focus:after,
+      &:active:after
+        width: 100%
       
-  //     li {
-  //       display: block;
-  //       height: 30%;
-  //       height: calc(100% / 3);
-  //       min-height: 50px;
-  //       position: relative;
-  //       opacity: 0;
-        
-  //       a {
-  //         display: block;
-  //         position: relative;
-  //         color: $color_link;
-  //         text-decoration: none;
-  //         overflow: hidden;
-  //         &:hover:after,
-  //          &:focus:after,
-  //          &:active:after {
-  //            width: 100%;
-  //          }
-          
-  //         &:after {
-  //           content: '';
-  //           position: absolute;
-  //           bottom: 0;
-  //           left: 50%;
-  //           width: 0%;
-  //           transform: translateX(-50%);
-  //           height: 3px;
-  //           background: $color_link;
-  //           transition: .35s;
-  //         }
-  //       }
-  //     }
-  //   }
-  // }
+      &:after
+        content: ''
+        position: absolute
+        bottom: 0
+        left: 50%
+        width: 0%
+        transform: translateX(-50%)
+        height: 3px
+        background: $color_link
+        transition: .35s
 
-  // // nav is open! see overlay
-  // .isNavOpen {
 
-  //   .btn-menu {
-  //     z-index: 200;
-  //     .top,
-  //     .middle,
-  //     .bottom {
-  //       background: $color_text;
-  //     }
-  //     .top {
-  //       transform: translateY(11px) translateX(0) rotate(45deg);
-  //     }
-  //     .middle {
-  //       opacity: 0;
-  //     }
-  //     .bottom {
-  //       transform: translateY(-11px) translateX(0) rotate(-45deg);
-  //     }
-  //   }
+  // nav is open! see overlay
+  .isNavOpen
 
-  //   .overlay {
-  //     opacity: 1;
-  //     visibility: visible;
-  //     height: 100%;
-      
-  //     li {
-  //       animation: fadeInRight .5s ease forwards;
-  //       animation-delay: .35s;
-        
-  //       &:nth-of-type(2) {
-  //         animation-delay: .4s;
-  //       }
-  //       &:nth-of-type(3) {
-  //         animation-delay: .45s;
-  //       }
-  //       &:nth-of-type(4) {
-  //         animation-delay: .50s;
-  //       }
-  //     }
-  //   }
-  // }
+    .btn-menu
+      z-index: 200
+      .btn-menu-line
+        background: $color_close_icon
+      .top
+        transform: translateY(11px) translateX(0) rotate(45deg)
+      .middle
+        opacity: 0
+      .bottom
+        transform: translateY(-11px) translateX(0) rotate(-45deg)
+      &:hover
+        .btn-menu-line
+          background: $color_close_icon_hvr
 
-  // @keyframes fadeInRight {
-  //   0% {
-  //     opacity: 0;
-  //     left: 20%;
-  //   }
-  //   100% {
-  //     opacity: 1;
-  //     left: 0;
-  //   }
-  // }
+    .overlay
+      opacity: 1
+      visibility: visible
+      height: 100%
+
+      li
+        animation: fadeInRight .5s ease forwards
+        animation-delay: .35s
+
+        &:nth-of-type(2)
+          animation-delay: .4s
+
+        &:nth-of-type(3)
+          animation-delay: .45s
+
+        &:nth-of-type(4)
+          animation-delay: .50s
+
+  @keyframes fadeInRight
+    0%
+      opacity: 0
+      left: 20%
+  
+    100%
+      opacity: 1
+      left: 0
 </style>
