61495c6ae886febc42b2b3237e757633c9cd7e1b
Valerio Sillari <valerio.sillari@gmail.com>
2017-12-16 02:40:36 +0100

    [skip ci] set styles for padding only for main sections

--- components/FooterArea.vue
+++ components/FooterArea.vue
@@ -16,7 +16,7 @@
 <style lang="sass">
   // todo: set later navi classes and styles via sass. now just dark one
   .footer
-    min-height: 200px
+    padding: 30px 0
     color: $color_footer_text
     background: $color_footer_bg
     border-top: 1px solid $color_footer_border
