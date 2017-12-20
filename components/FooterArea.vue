eb36430d6cfd7d8302187c1b95c18c1576c4886d
Valerio Sillari <valerio.sillari@gmail.com>
2017-12-20 12:23:09 +0100

    [skip ci] footer. set basic grid and text. to do: icons on right side.

--- components/FooterArea.vue
+++ components/FooterArea.vue
@@ -1,13 +1,22 @@
 <template lang="pug">
   .footer
     .container
-      | Footer here
+      .row
+        .col-10.col-sm-8.col-left
+          p.text
+            | Zitronen Strasse | {{ dateYear }} | A project by
+            | &nbsp;
+            a.item-link(href='http://www.valeriosillari.com/', title='Valerio Sillari', target='_blank')
+              | Valerio Sillari
+        .col-sm-4.col-right
+          | here area icons
 </template>
 
 <script>
   export default {
     data () {
       return {
+        dateYear: new Date().getFullYear()
       }
     }
   }
@@ -16,8 +25,29 @@
 <style lang="sass">
   // todo: set later navi classes and styles via sass. now just dark one
   .footer
+    font-size: 14px
     padding: 30px 0
     color: $color_footer_text
     background: $color_footer_bg
     border-top: 1px solid $color_footer_border
+
+  .text
+    margin-bottom: .05rem
+
+  .item-link
+    color: $color_footer_link
+    &:hover,
+    &:focus
+      color: $color_footer_link_hvr
+
+  .col-right
+    background: pink
+
+  // SM: tablet
+  @media screen and (min-width: 576px)
+
+    .col-right
+      text-align: right
+      background: red
+
 </style>
