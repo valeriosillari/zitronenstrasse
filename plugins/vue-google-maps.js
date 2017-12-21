0da8439c291433fb2ab86c92ffcfbeabddc7d7eb
valerio sillari <valerio.sillari@gmail.com>
2017-12-21 23:52:39 +0100

    DEPLOY: found a solution. set vue map import as in issue comment. locally i build the application. trying ... https://github.com/xkjyeah/vue-google-maps/issues/217#issuecomment-335912985

--- plugins/vue-google-maps.js
+++ plugins/vue-google-maps.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import * as VueGoogleMaps from '~/node_modules/vue2-google-maps/src/main'
+import * as VueGoogleMaps from '~/node_modules/vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
   load: {
