26d731297edbdddcfe46131fb56fef2dbc2c144a
valerio sillari <valerio.sillari@gmail.com>
2017-12-22 00:38:03 +0100

    ok, start again. we see map. dev works but build has error.

--- plugins/vue-google-maps.js
+++ plugins/vue-google-maps.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import * as VueGoogleMaps from '~/node_modules/vue2-google-maps'
+import * as VueGoogleMaps from '~/node_modules/vue2-google-maps/src/main'
 
 Vue.use(VueGoogleMaps, {
   load: {
