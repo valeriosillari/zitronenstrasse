c6eb3f450b4e97634ae346256fc6ad29699c2dc5
valerio sillari <valerio.sillari@gmail.com>
2017-12-21 17:26:26 +0100

    added more places. looks we can have an array with MAX 10 places. we have to investigate.

--- components/MapGoogle/_placesIdArrays.js
+++ components/MapGoogle/_placesIdArrays.js
@@ -1,12 +1,47 @@
 const placeIdArray = [
-  // tier
-  'ChIJOSWgYaVPqEcR63RnoNfgsDg',
-  // b-lage
-  'ChIJAV_YIZ5PqEcRKxI0jRwb58I',
   // Geschwister Nothaft Café
   'ChIJX2MutgxPqEcR2nvgHiQ0ojE',
+  // b-lage
+  'ChIJAV_YIZ5PqEcRKxI0jRwb58I',
   // Herr Lindemann
-  'ChIJd-_qBqBPqEcRhdYalX3ASNI'
+  'ChIJd-_qBqBPqEcRhdYalX3ASNI',
+  // Hallmann und Klee
+  'ChIJa6BtJAtPqEcRfVVCtKgmXAA',
+  // Cafebar Mal so Mal so
+  'ChIJIStVHgtPqEcRpj_scwDgiho',
+  // Körner Park
+  'ChIJTRJ0JZ9PqEcRABns9MHCtoA',
+  // DAS KAPiTAL
+  'ChIJdT1vJqBPqEcRKr3fqKwU5gI',
+  // balera Weinhandlung
+  'ChIJyaJmgJ9PqEcRmFMyFfFqJGQ',
+  // Vux
+  'ChIJC-mZa6BPqEcRMXhPH5jFJyo',
+  // tier
+  'ChIJOSWgYaVPqEcR63RnoNfgsDg'
+  // // CocoLiebe
+  // 'ChIJy91K4KNPqEcRCAd6KD6oZx8',
+  // // Sameheads
+  // 'ChIJn5Np3qNPqEcRrRkTsDUHeEg',
+  // // Ä
+  // 'ChIJz61EdaZPqEcRZb1ZzQXwx_4',
+  // // Roamers
+  // 'ChIJ_UQ2CbpPqEcRzhfVydN_PCs',
+  // // Vin Aqua Vin
+  // 'ChIJ06r-0LBPqEcR9tmvBjs5ab0',
+  // // Damensalon
+  // 'ChIJp0sppLZPqEcRasG_5n9uw3A',
+  // // Fuks Bar
+  // 'ChIJIzn297ZPqEcRgw6RoWPK3GU',
+  // // Myxa Berlin
+  // 'ChIJn6jqirZPqEcRSTM-cJbVGAY',
+  // // Geist im Glas
+  // 'ChIJo99QRrZPqEcRXW-E2PeTJ4A',
+  // // Fuchsbau
+  // 'ChIJRSQRf8tPqEcRw5esshbdL8k',
+  // // Pony Saloon Berlin
+  // 'ChIJqwCqdclPqEcRYUkH0bnBeFk'
+
 ]
 
 export default placeIdArray
