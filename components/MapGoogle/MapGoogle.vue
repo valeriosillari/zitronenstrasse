6818280f1fe1a124c029f22446b262704a1f77a8
valerio sillari <valerio.sillari@gmail.com>
2017-12-21 19:42:34 +0100

    [skip ci] set basic markup for container. and styles.

--- components/MapGoogle/MapGoogle.vue
+++ components/MapGoogle/MapGoogle.vue
@@ -1,55 +1,22 @@
-<template>
-  <gmap-map
-    class="google-map"
-    :center='mapCentering'
-    :zoom='zoomOption'
-    :options='options'
-  >
-
-    <gmap-marker
-      :key="index"
-      v-for="(m, index) in markers"
-      :position="m.position"
-    >
-    </gmap-marker>
-
-  </gmap-map>
+<template lang="pug">
+  .map-main-wrapper
+    .google-map#google-map
 </template>
 
 <script>
   export default {
     data () {
       return {
-        mapCentering: {
-          lat: 52.482841,
-          lng: 13.4252209
-        },
-        zoomOption: 14,
-        // map options
-        options: {
-          streetViewControl: false,
-          fullscreenControl: false,
-          mapTypeControl: false
-          // set custom map styles
-          // styles: mapStylesDark
-        },
-        markers: [{
-          position: {
-            lat: 52.482845,
-            lng: 13.438589
-          }
-        }, {
-          position: {
-            lat: 52.478430,
-            lng: 13.439651
-          }
-        }]
       }
     }
   }
 </script>
 
 <style lang="sass">
+  .map-main-wrapper
+    width: 100%
+    min-height: 100%
+
   .google-map
     position: absolute
     width: 100%
