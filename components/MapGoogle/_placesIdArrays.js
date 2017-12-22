5fbb37c1b03f66099e7d5a2a6815d4cc5654af9c
valerio sillari <valerio.sillari@gmail.com>
2017-12-23 00:53:51 +0100

    TEST: removed places. testing speed insight.

--- components/MapGoogle/_placesIdArrays.js
+++ components/MapGoogle/_placesIdArrays.js
@@ -1,24 +1,27 @@
 const placeIdArray = [
-  // Geschwister Nothaft Café
-  'ChIJX2MutgxPqEcR2nvgHiQ0ojE',
-  // b-lage
-  'ChIJAV_YIZ5PqEcRKxI0jRwb58I',
-  // Herr Lindemann
-  'ChIJd-_qBqBPqEcRhdYalX3ASNI',
-  // Hallmann und Klee
-  'ChIJa6BtJAtPqEcRfVVCtKgmXAA',
-  // Cafebar Mal so Mal so
-  'ChIJIStVHgtPqEcRpj_scwDgiho',
-  // Körner Park
-  'ChIJTRJ0JZ9PqEcRABns9MHCtoA',
-  // DAS KAPiTAL
-  'ChIJdT1vJqBPqEcRKr3fqKwU5gI',
-  // balera Weinhandlung
-  'ChIJyaJmgJ9PqEcRmFMyFfFqJGQ',
-  // Vux
-  'ChIJC-mZa6BPqEcRMXhPH5jFJyo',
-  // tier
-  'ChIJOSWgYaVPqEcR63RnoNfgsDg'
+  // // Geschwister Nothaft Café
+  // 'ChIJX2MutgxPqEcR2nvgHiQ0ojE',
+  // // b-lage
+  // 'ChIJAV_YIZ5PqEcRKxI0jRwb58I',
+  // // Herr Lindemann
+  // 'ChIJd-_qBqBPqEcRhdYalX3ASNI',
+  // // Hallmann und Klee
+  // 'ChIJa6BtJAtPqEcRfVVCtKgmXAA',
+  // // Cafebar Mal so Mal so
+  // 'ChIJIStVHgtPqEcRpj_scwDgiho',
+  // // Körner Park
+  // 'ChIJTRJ0JZ9PqEcRABns9MHCtoA',
+  // // DAS KAPiTAL
+  // 'ChIJdT1vJqBPqEcRKr3fqKwU5gI',
+  // // balera Weinhandlung
+  // 'ChIJyaJmgJ9PqEcRmFMyFfFqJGQ',
+  // // Vux
+  // 'ChIJC-mZa6BPqEcRMXhPH5jFJyo',
+  // // tier
+  // 'ChIJOSWgYaVPqEcR63RnoNfgsDg'
+
+  // ------------------------------------------
+  // more than 10
   // // CocoLiebe
   // 'ChIJy91K4KNPqEcRCAd6KD6oZx8',
   // // Sameheads
