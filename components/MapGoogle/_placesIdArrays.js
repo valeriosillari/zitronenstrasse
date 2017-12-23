23583f68f76f96f16bc34c80885e9c8a5ef5fdd4
valerio sillari <valerio.sillari@gmail.com>
2017-12-23 01:10:49 +0100

    TEST: set places again. they effect page speed. basically the cache problem.

--- components/MapGoogle/_placesIdArrays.js
+++ components/MapGoogle/_placesIdArrays.js
@@ -1,27 +1,28 @@
 const placeIdArray = [
-  // // Geschwister Nothaft Café
-  // 'ChIJX2MutgxPqEcR2nvgHiQ0ojE',
-  // // b-lage
-  // 'ChIJAV_YIZ5PqEcRKxI0jRwb58I',
-  // // Herr Lindemann
-  // 'ChIJd-_qBqBPqEcRhdYalX3ASNI',
-  // // Hallmann und Klee
-  // 'ChIJa6BtJAtPqEcRfVVCtKgmXAA',
-  // // Cafebar Mal so Mal so
-  // 'ChIJIStVHgtPqEcRpj_scwDgiho',
-  // // Körner Park
-  // 'ChIJTRJ0JZ9PqEcRABns9MHCtoA',
-  // // DAS KAPiTAL
-  // 'ChIJdT1vJqBPqEcRKr3fqKwU5gI',
-  // // balera Weinhandlung
-  // 'ChIJyaJmgJ9PqEcRmFMyFfFqJGQ',
-  // // Vux
-  // 'ChIJC-mZa6BPqEcRMXhPH5jFJyo',
-  // // tier
-  // 'ChIJOSWgYaVPqEcR63RnoNfgsDg'
+  // Geschwister Nothaft Café
+  'ChIJX2MutgxPqEcR2nvgHiQ0ojE',
+  // b-lage
+  'ChIJAV_YIZ5PqEcRKxI0jRwb58I',
+  // Herr Lindemann
+  'ChIJd-_qBqBPqEcRhdYalX3ASNI',
+  // Hallmann und Klee
+  'ChIJa6BtJAtPqEcRfVVCtKgmXAA',
+  // Cafebar Mal so Mal so
+  'ChIJIStVHgtPqEcRpj_scwDgiho',
+  // Körner Park
+  'ChIJTRJ0JZ9PqEcRABns9MHCtoA',
+  // DAS KAPiTAL
+  'ChIJdT1vJqBPqEcRKr3fqKwU5gI',
+  // balera Weinhandlung
+  'ChIJyaJmgJ9PqEcRmFMyFfFqJGQ',
+  // Vux
+  'ChIJC-mZa6BPqEcRMXhPH5jFJyo',
+  // tier
+  'ChIJOSWgYaVPqEcR63RnoNfgsDg'
 
   // ------------------------------------------
   // more than 10
+
   // // CocoLiebe
   // 'ChIJy91K4KNPqEcRCAd6KD6oZx8',
   // // Sameheads
@@ -42,6 +43,10 @@ const placeIdArray = [
   // 'ChIJo99QRrZPqEcRXW-E2PeTJ4A',
   // // Fuchsbau
   // 'ChIJRSQRf8tPqEcRw5esshbdL8k',
+
+  // ------------------------------------------
+  // more than 20
+
   // // Pony Saloon Berlin
   // 'ChIJqwCqdclPqEcRYUkH0bnBeFk'
 
