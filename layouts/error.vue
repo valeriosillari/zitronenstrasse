09c0e6ca3f1436a0472ff300d61fe62bade27ec5
valerio sillari <valerio.sillari@gmail.com>
2018-01-04 01:16:58 +0100

    [skip ci] set basic error page page. https://nuxtjs.org/guide/views/#layouts https://raw.githubusercontent.com/nuxt/nuxt.js/master/lib/app/components/nuxt-error.vue

---
+++ layouts/error.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+  .layout-wrap
+
+    //- navbar
+    Navbar
+
+    //- main area
+    .container
+      section.section.section-error(style='min-height: 1200px;')
+        h1.error-code
+          | {{ error.statusCode }}
+
+        .error-wrapper-message
+          h2.error-message
+            | {{ error.message }}
+
+        p(v-if='error.statusCode === 404')
+          nuxt-link.error-link(to="/")
+            | Back to the home page
+
+    //- footer
+    FooterArea
+</template>
+
+<script>
+  import Navbar from '~/components/Navbar.vue'
+  import FooterArea from '~/components/FooterArea.vue'
+
+  export default {
+    components: {
+      Navbar,
+      FooterArea
+    },
+    name: 'nuxt-error',
+    props: ['error'],
+    head () {
+      return {
+        title: this.error.message || 'An error occured'
+      }
+    }
+  }
+</script>
