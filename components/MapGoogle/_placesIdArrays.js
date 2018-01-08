f190c020d91f5c610b113dc145bc9660df81151c
Valerio Sillari <valerio.sillari@gmail.com>
2018-01-08 11:46:10 +0100

    Test: array of markers is now empty (commented). we want to test speed

--- components/MapGoogle/_placesIdArrays.js
+++ components/MapGoogle/_placesIdArrays.js
@@ -1,53 +1,53 @@
 const placeIdArray = [
-  // 001 - Geschwister Nothaft Café
-  'ChIJX2MutgxPqEcR2nvgHiQ0ojE',
-  // 002 - b-lage
-  'ChIJAV_YIZ5PqEcRKxI0jRwb58I',
-  // 003 - Herr Lindemann
-  'ChIJd-_qBqBPqEcRhdYalX3ASNI',
-  // 004 - Hallmann und Klee
-  'ChIJa6BtJAtPqEcRfVVCtKgmXAA',
-  // 005 - Cafebar Mal so Mal so
-  'ChIJIStVHgtPqEcRpj_scwDgiho',
-  // 006 - Körner Park
-  'ChIJTRJ0JZ9PqEcRABns9MHCtoA',
-  // 007 - DAS KAPiTAL
-  'ChIJdT1vJqBPqEcRKr3fqKwU5gI',
-  // 008 - balera Weinhandlung
-  'ChIJyaJmgJ9PqEcRmFMyFfFqJGQ',
-  // 009 - Vux
-  'ChIJC-mZa6BPqEcRMXhPH5jFJyo',
-  // 010 - tier
-  'ChIJOSWgYaVPqEcR63RnoNfgsDg'
+  // // 001 - Geschwister Nothaft Café
+  // 'ChIJX2MutgxPqEcR2nvgHiQ0ojE',
+  // // 002 - b-lage
+  // 'ChIJAV_YIZ5PqEcRKxI0jRwb58I',
+  // // 003 - Herr Lindemann
+  // 'ChIJd-_qBqBPqEcRhdYalX3ASNI',
+  // // 004 - Hallmann und Klee
+  // 'ChIJa6BtJAtPqEcRfVVCtKgmXAA',
+  // // 005 - Cafebar Mal so Mal so
+  // 'ChIJIStVHgtPqEcRpj_scwDgiho',
+  // // 006 - Körner Park
+  // 'ChIJTRJ0JZ9PqEcRABns9MHCtoA',
+  // // 007 - DAS KAPiTAL
+  // 'ChIJdT1vJqBPqEcRKr3fqKwU5gI',
+  // // 008 - balera Weinhandlung
+  // 'ChIJyaJmgJ9PqEcRmFMyFfFqJGQ',
+  // // 009 - Vux
+  // 'ChIJC-mZa6BPqEcRMXhPH5jFJyo',
+  // // 010 - tier
+  // 'ChIJOSWgYaVPqEcR63RnoNfgsDg'
 
   // ------------------------------------------
   // more than 10
 
-  // // CocoLiebe
+  // // 011 - CocoLiebe
   // 'ChIJy91K4KNPqEcRCAd6KD6oZx8',
-  // // Sameheads
+  // // 012 - Sameheads
   // 'ChIJn5Np3qNPqEcRrRkTsDUHeEg',
-  // // Ä
+  // // 013 - Ä
   // 'ChIJz61EdaZPqEcRZb1ZzQXwx_4',
-  // // Roamers
+  // // 014 - Roamers
   // 'ChIJ_UQ2CbpPqEcRzhfVydN_PCs',
-  // // Vin Aqua Vin
+  // // 015 - Vin Aqua Vin
   // 'ChIJ06r-0LBPqEcR9tmvBjs5ab0',
-  // // Damensalon
+  // // 016 - Damensalon
   // 'ChIJp0sppLZPqEcRasG_5n9uw3A',
-  // // Fuks Bar
+  // // 017 - Fuks Bar
   // 'ChIJIzn297ZPqEcRgw6RoWPK3GU',
-  // // Myxa Berlin
+  // // 018 - Myxa Berlin
   // 'ChIJn6jqirZPqEcRSTM-cJbVGAY',
-  // // Geist im Glas
+  // // 019 - Geist im Glas
   // 'ChIJo99QRrZPqEcRXW-E2PeTJ4A',
-  // // Fuchsbau
+  // // 020 - Fuchsbau
   // 'ChIJRSQRf8tPqEcRw5esshbdL8k',
 
   // ------------------------------------------
   // more than 20
 
-  // // Pony Saloon Berlin
+  // // 021 - Pony Saloon Berlin
   // 'ChIJqwCqdclPqEcRYUkH0bnBeFk'
 ]
 
