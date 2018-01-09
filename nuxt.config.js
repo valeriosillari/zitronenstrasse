9f111e56c552cce8fbd97ff3b129f34a67bf3add
valerio sillari <valerio.sillari@gmail.com>
2018-01-10 00:57:41 +0100

    update meta description.

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ const webpack = require('webpack')
 const mapApiKey = process.env.GOOGLE_MAP_API_KEY || ''
 
 const headTitle = 'Zitronenstrasse | Romantic Spots in Berlin.'
-const headDescription = 'Zitronenstrasse | Romantic Spots in Berlin.'
+const headDescription = 'A project for mapping and sharing romantic spots in Berlin.'
 
 // TODO: the URL set as variables check for redirect DNS
 const thisAppMainUrl = 'https://zitronenstrasse.herokuapp.com/'
