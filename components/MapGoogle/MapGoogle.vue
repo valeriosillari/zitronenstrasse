75e8999c9399ff86c9c093fa05f8df0ef99f448f
Valerio Sillari <valerio.sillari@gmail.com>
2018-01-15 12:03:35 +0100

    [skip ci] set custom marker design. todo: infowindow

--- components/MapGoogle/MapGoogle.vue
+++ components/MapGoogle/MapGoogle.vue
@@ -7,6 +7,16 @@
   import placeIdArray from '~/components/MapGoogle/_placesIdArrays.js'
   import mapStylesDark from '~/components/MapGoogle/_mapStylesDark.js'
 
+  // marker custom colors
+  // custom color: a little bit darker then the main one. for the marker looks the same
+  const markerColorFill = '#eab622'
+  const markerColorStroke = '#FFC832'
+  // path inspiration from this codepen:
+  // https://codepen.io/defvayne23/pen/EVYGRw?editors=1010
+  // we follow the svg path to amazon and taken the path from the original url
+  // https://s3-us-west-2.amazonaws.com/s.cdpn.io/134893/pin-red.svg
+  const markerPath = 'M 8 2.1 c 1.1 0 2.2 0.5 3 1.3 c 0.8 0.9 1.3 1.9 1.3 3.1 s -0.5 2.5 -1.3 3.3 l -3 3.1 l -3 -3.1 c -0.8 -0.8 -1.3 -2 -1.3 -3.3 c 0 -1.2 0.4 -2.2 1.3 -3.1 c 0.8 -0.8 1.9 -1.3 3 -1.3 Z'
+
   export default {
     data () {
       // empty object
@@ -26,6 +36,17 @@
         // timer for avoiding TIMEOUT: 450 looks fine, below errors ...
         const timer = indexNumber * 425
 
+        const customMarker = {
+          path: markerPath,
+          fillColor: markerColorFill,
+          strokeColor: markerColorStroke,
+          fillOpacity: 1,
+          scale: 2.5,
+          strokeWeight: 2,
+          // for correct alignmnet of custom SVG icon with map point
+          anchor: new google.maps.Point(11, 12)
+        }
+
         // timeout playing time
         setTimeout(() => {
           new google.maps.places.PlacesService(mapLoaded).getDetails({
@@ -37,8 +58,9 @@
                 place: {
                   placeId: placeID,
                   location: result.geometry.location
-                }
-                // TODO: set icon custom style
+                },
+                // set icon custom style
+                icon: customMarker
               })
 
               return marker
