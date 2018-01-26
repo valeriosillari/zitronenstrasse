ff7eca433c02c446a24fffe85675e730a7515a28
Valerio Sillari <valerio.sillari@gmail.com>
2018-01-26 16:25:28 +0100

    [skip ci]: added again logic for custom marker design (yellow marker)

--- components/MapGoogle/MapGoogle.vue
+++ components/MapGoogle/MapGoogle.vue
@@ -86,6 +86,28 @@
       // !!! we need this google constant
       const google = window.google
 
+      // marker custom colors
+      // custom color: a little bit darker then the main one. for the marker looks the same
+      const markerColorFill = '#eab622'
+      const markerColorStroke = '#FFC832'
+
+      // path inspiration from this codepen:
+      // https://codepen.io/defvayne23/pen/EVYGRw?editors=1010
+      // we follow the svg path to amazon and taken the path from the original url
+      // https://s3-us-west-2.amazonaws.com/s.cdpn.io/134893/pin-red.svg
+      const markerPath = 'M 8 2.1 c 1.1 0 2.2 0.5 3 1.3 c 0.8 0.9 1.3 1.9 1.3 3.1 s -0.5 2.5 -1.3 3.3 l -3 3.1 l -3 -3.1 c -0.8 -0.8 -1.3 -2 -1.3 -3.3 c 0 -1.2 0.4 -2.2 1.3 -3.1 c 0.8 -0.8 1.9 -1.3 3 -1.3 Z'
+
+      const customMarker = {
+        path: markerPath,
+        fillColor: markerColorFill,
+        strokeColor: markerColorStroke,
+        fillOpacity: 1,
+        scale: 2.5,
+        strokeWeight: 2,
+        // for correct alignmnet of custom SVG icon with map point
+        anchor: new google.maps.Point(11, 12)
+      }
+
       const initMapCanvas = () => {
         // init map
         this.map = new google.maps.Map(document.getElementById('google-map'), {
@@ -129,7 +151,9 @@
             // ========= set marker API data =========
             let marker = new google.maps.Marker({
               map: this.map,
-              position: result.geometry.location
+              position: result.geometry.location,
+              // set icon custom style
+              icon: customMarker
             })
 
             // ========= set marker Infowindow =========
