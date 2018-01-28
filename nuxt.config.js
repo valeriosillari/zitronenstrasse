721dd85b09b30a0512efecde41279f8cdf95d13b
valerio sillari <valerio.sillari@gmail.com>
2018-01-28 01:17:00 +0100

    DEPLOY: test only with async

--- nuxt.config.js
+++ nuxt.config.js
@@ -175,7 +175,7 @@ module.exports = {
     script: [
       {
         // call google map javascript API
-        src: `https://maps.googleapis.com/maps/api/js?key=${mapApiKey}&v=3&libraries=places`
+        src: `https://maps.googleapis.com/maps/api/js?key=${mapApiKey}&v=3&libraries=places`, async: true
       }
     ]
   },
