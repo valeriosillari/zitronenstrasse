07cf51e8eb36c0df2d65b7428d8f8d32cc316797
valerio sillari <valerio.sillari@gmail.com>
2018-01-28 16:24:41 +0100

    [skip ci] set basic text for about

--- pages/about.vue
+++ pages/about.vue
@@ -2,11 +2,104 @@
   section.section.section-top
     .container
       h1
-        | The project
+        | About
 
       p
-        | Zitronenstrasse is a small project for collecting and sharing romantic spots in Berlin. Bars, cafes, restaurants, parks and some little hidden places around the city.
+        span.text-accent
+          | Zitronenstrasse
+        | &nbsp;
+        | is a small project for collecting and sharing romantic and cozy spots in Berlin.
 
       p
-        | For whom is new in city, for whom wanting discover some new places, for whom is looking a romantic spot for a special date or just for having an inspirational and different guide in Berlin.
+        | Bars, cafes, restaurants, bistros, parks and some little hidden places around the city.
+
+      //- ====================
+      h2
+        | The Project
+
+      p
+        | Berlin is wonderful city. So vivid, multicultural, full of history and so many secret places.
+
+      p
+        | However,
+        | &nbsp;
+        span.text-accent
+          | sometimes finding the right place for the right mood is so hard
+        | , even harder when you are looking for a romantic place. It has to be nice, but not so much cozy; it has to have nice music, but not too much loud; nice drinks and menu, but nothing too much classic; with nice people, but not too much crowd.
+
+      p
+        | So here you can find a small selection of romantic spots: some of them are perfect for a brunch or a coffee, ones for the evening with drinks and music, others are bars or outdoors places where drinking a beer and waiting for the sunset.
+      
+      P
+        | Hopefully you will find here your romantic place.
+
+      //- ====================
+      h2
+        | The Name
+
+      p
+        span.text-accent
+          | Why Zitronenstrasse?
+        | &nbsp;
+        | It is just a name of a street? A real one? What does it means?
+      
+      p
+        | Well, here the story ...
+
+      p
+        | Basically Zitronenstrasse is an
+        | &nbsp;
+        span.text-accent
+          | inside joke between two languages
+        |, German and Italian.
+
+      p
+        | In Italian there is slang word that means
+        | &nbsp;
+        span.text-accent
+          | making out
+        | :
+        | &nbsp;
+        i
+          | 'limonare'
+        | , from the word
+        | &nbsp;
+        i
+          | 'limone'
+        | &nbsp;
+        | (lemon).
+
+      p
+        | So we mixed this word with the German name for
+        | &nbsp;
+        span.text-accent
+          | street
+        | &nbsp;
+        | (
+        i
+          | Strasse
+        | &nbsp;
+        | or 
+        | &nbsp;
+        i
+          | Straße
+        | ). And so, we got Zitronenstrasse: the romantic place for making out ...
+
+      //- ====================
+      h2
+        | The Author
+
+      p
+        | I am
+        | &nbsp;
+        span.text-accent
+          | Valerio
+        |. I am working as Frontend Developer and in the last years I have fallen in love with the Berlin.
+
+      p
+        | So I decided to create this website for collecting all the nice romantic places I have found and also playing with new frontend languages for my work.
+
+      p
+        | I hope you like it, either you are a romantic person or a romantic coder.
+
 </template>
