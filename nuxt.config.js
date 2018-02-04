ba4f2c0df3d2adeb49d0f672f19e059caf4af879
valerio sillari <valerio.sillari@gmail.com>
2018-02-04 19:00:17 +0100

    DEPLOY: set js also defer for map.

--- nuxt.config.js
+++ nuxt.config.js
@@ -176,12 +176,11 @@ module.exports = {
       {
         // call google map javascript API
         src: `https://maps.googleapis.com/maps/api/js?key=${mapApiKey}&v=3`,
+        async: true,
+        defer: true,        
         // set js at end of body:
         // https://github.com/nuxt/nuxt.js/issues/241
-        body: true,
-        // TODO: check nuxt logic for async. we got issue with map
-        async: true
-        // defer: true
+        body: true
       }
     ]
   },
