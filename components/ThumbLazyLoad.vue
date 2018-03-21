9ef36d264f7f5ef868005e9933fc281e63ad788c
Valerio Sillari <valerio.sillari@gmail.com>
2018-03-21 13:34:04 +0100

    [skip ci] cleanup code pug

--- components/ThumbLazyLoad.vue
+++ components/ThumbLazyLoad.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
-    div.line(v-if='infoThumbLazyLoad.thumb')
-      img.thumb(
-        :src='infoThumbLazyLoad.thumb'
-        :alt='infoThumbLazyLoad.title'
-      )
+    img.thumb(
+      v-if='infoThumbLazyLoad.thumb'
+      :src='infoThumbLazyLoad.thumb'
+      :alt='infoThumbLazyLoad.title'
+    )
     //- fallback
-    div.line(v-else='infoThumbLazyLoad.thumb')
-      img.thumb(
-        src='/thumb/fallback/440x250_fallback_magenta.png'
-        :alt='infoThumbLazyLoad.title'
-      )
+    img.thumb(
+      v-else='infoThumbLazyLoad.thumb'
+      src='/thumb/fallback/440x250_fallback_magenta.png'
+      :alt='infoThumbLazyLoad.title'
+    )
 </template>
 
 
