1f7afc91f21404018e053b196327ae9d2b174856
Valerio Sillari <valerio.sillari@gmail.com>
2018-04-06 09:51:44 +0200

    [skip ci] set test page and layout form images.

---
+++ layouts/testing.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  .layout-wrapper
+    //- nuxt tag
+    nuxt
+</template>
+
+<script>
+  export default {
+    // mounted: WHEN ALL code on server is already loaded!
+    mounted () {
+      console.log('TESTING - 001')
+    }
+  }
+</script>
---
+++ pages/testme.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  section.section.section-text
+    .container
+      h1
+        | Test
+</template>
+
+
+<script>
+  export default {
+    layout: 'testing',
+    head () {
+      // Set Meta Tags for this Page
+      return {
+        title: 'TEST ME',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: `About | Zitronenstrasse | A mapping project to collect and share romantic spots in Berlin.`
+          }
+        ]
+      }
+    }
+  }
+</script>
