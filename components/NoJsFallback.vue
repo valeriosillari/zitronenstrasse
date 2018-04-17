9e4500da6270c160bd43a0a0b33ceb199d623fdc
Valerio Sillari <valerio.sillari@gmail.com>
2018-04-17 11:55:40 +0200

    [skip ci] no js : start setting a overlay for NO js user with also css anmation for fade in + delay follow this tutorial: https://jsfiddle.net/75mhnaLt/

---
+++ components/NoJsFallback.vue
@@ -0,0 +1,40 @@
+<template lang="pug">
+  .no-js-fallback
+    .content
+      | Sorry you got no JS
+</template>
+
+
+<style lang="sass">
+
+  @keyframes fadein
+    0%
+      opacity: 0
+    66%
+      opacity: 0
+    100%
+      opacity: 1
+
+  .no-js-fallback
+    display: none
+
+  .no-js
+    .no-js-fallback
+      display: flex
+      align-items: center
+      justify-content: center
+      position: fixed
+      z-index: 100000
+      top: 0
+      bottom: 0
+      width: 100%
+      background: red
+      color: white
+
+      // fade in animation
+      animation: 3s ease 0s normal forwards 1 fadein
+
+      // content area: centered by flexbox
+      .content
+        max-width: 50%
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,20 +2,23 @@
   .layout-wrapper
     //- navbar
     Navbar
-    //- nuxt tag
+    //- nuxt tag (page core)
     nuxt
     //- footer
     FooterArea
+    //- no js fallback
+    NoJsFallback
 </template>
 
 <script>
   import Navbar from '~/components/Navbar.vue'
   import FooterArea from '~/components/FooterArea.vue'
-
+  import NoJsFallback from '~/components/NoJsFallback.vue'
   export default {
     components: {
       Navbar,
-      FooterArea
+      FooterArea,
+      NoJsFallback
     },
     // mounted: WHEN ALL code on server is already loaded!
     mounted () {
