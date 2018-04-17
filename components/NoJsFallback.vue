edad72e896b14dc9971ee385870315bdbfe5650a
Valerio Sillari <valerio.sillari@gmail.com>
2018-04-17 13:48:30 +0200

    [skip ci] no js : set better code for no js feedback message. we have to set a close button for not blocking the usage of the website

--- components/NoJsFallback.vue
+++ components/NoJsFallback.vue
@@ -1,7 +1,18 @@
 <template lang="pug">
   .no-js-fallback
     .content
-      | Sorry you got no JS
+      p.h2
+        | Ops!
+      p
+        | Looks like you have Javascript disabled. 😮 😮 😮
+      p
+        | To enable it follow
+        | &nbsp;
+        a(href='https://enable-javascript.com/', target='_blank', title='Enable Javascript')
+          | this link
+      p
+        | You can also continue watching the website without Javascript, but you will lose a lot of fun ...
+      //- set here BTN close with css logic only (checkbox)
 </template>
 
 
@@ -28,13 +39,15 @@
       top: 0
       bottom: 0
       width: 100%
-      background: red
-      color: white
+      background: $color_fallback_bg
 
       // fade in animation
       animation: 3s ease 0s normal forwards 1 fadein
 
       // content area: centered by flexbox
       .content
-        max-width: 50%
+        max-width: 90%
+        color: $color_fallback_text
+        +breakpoint($breakpoint_sm)
+          max-width: 50%
 </style>
