d961299fbc1b1ba09067990dae503a9949565293
Valerio Sillari <valerio.sillari@gmail.com>
2018-05-23 23:25:17 +0200

    [skip ci] set privacy page with dummy text. we have to set it.

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  section.section.section-text
+    .container
+      h1
+        | Privacy Policy
+
+      p
+        | Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur perspiciatis dignissimos, ad? Nostrum vero eligendi quos temporibus, debitis, molestiae odio repellat enim iusto ipsa repellendus, quo eaque earum, hic laudantium!
+</template>
+
+
+<script>
+  export default {
+    head () {
+      // Set Meta Tags for this Page
+      return {
+        title: 'Privacy Policy | Zitronenstrasse',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: `Privacy Policy | Zitronenstrasse | ${process.env.metaHeadDescription}`
+          }
+        ]
+      }
+    }
+  }
+</script>
