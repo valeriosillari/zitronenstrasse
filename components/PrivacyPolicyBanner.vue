80de5aac8782f83595d56004fb210f4b25473243
Valerio Sillari <valerio.sillari@gmail.com>
2018-05-24 16:54:24 +0200

    DEV-2786: set pug template. colors : I'm not so sure but ok for now

--- components/PrivacyPolicyBanner.vue
+++ components/PrivacyPolicyBanner.vue
@@ -1,14 +1,13 @@
-<template>
-  <no-ssr>
-    <div>
-      <cookie-law theme='lemon-theme' buttonText='I agree'>
-        <div slot="message">
-          This site uses some cookies in order to count visits. By clicking the 'I agree' button you give your consent.
-          More info here about Privacy Policy <router-link to='/privacy-policy' title="Privacy Policy Info">here</router-link>.
-        </div>
-      </cookie-law>
-    </div>
-  </no-ssr>
+<template lang="pug">
+  no-ssr
+    cookie-law(theme='lemon-theme' buttonText='I agree')
+      div(slot='message')
+        | This site uses some cookies in order to count visits. By clicking the 'I agree' button you give your consent.
+        | More info here about Privacy Policy
+        |
+        nuxt-link(to='/privacy-policy' title='Privacy Policy Info')
+          | here
+        |.
 </template>
 
 
