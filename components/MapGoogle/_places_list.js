47ca8c2f8b1764d1a40a29eddb6a1adf300fc0c2
Valerio Sillari <valerio.sillari@gmail.com>
2018-07-15 19:31:49 +0200

    [skip ci] set basic 2 places. to finish

--- components/MapGoogle/_places_list.js
+++ components/MapGoogle/_places_list.js
@@ -751,6 +751,32 @@ const placesList = [
       lat: 52.502094,
       lng: 13.443147
     }
+  },
+  {
+    // 056
+    // ChIJIzn7BchPqEcRF5uxEeEl-JE
+    title: 'Zosse',
+    address: 'Richardstraße 37, 12043',
+    thumb: '/thumbs/places/056_zosse.jpg',
+    thumbCredits: 'Zosse FB Page',
+    fbPage: 'https://www.facebook.com/ZosseBar/',
+    position: {
+      lat: 52.4749825,
+      lng: 13.4441024
+    }
+  },
+  {
+    // 057
+    // ChIJe-JZ6sRPqEcR57AEB-9Vg90
+    title: 'Floating University Berlin',
+    address: 'Lilienthalstraße 32, 10965 ',
+    thumb: '/thumbs/places/057_floating_university_berlin.jpg',
+    thumbCredits: 'Zitronenstrasse',
+    website: 'http://www.floatinguniversity.org/',
+    position: {
+      lat: 52.4847201,
+      lng: 13.4065031
+    }
   }
 ]
 
