15eb5469253ae9d9bbb8ed39a05e4243e2cb33d3
Valerio Sillari <valerio.sillari@gmail.com>
2018-08-24 13:35:42 +0200

    [skip ci] we use correct API key from DotEnv

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -3,7 +3,7 @@ import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'YOUR_API_TOKEN',
+    key: googleMapApiKey,
     libraries: 'places', // This is required if you use the Autocomplete plugin
     // OR: libraries: 'places,drawing'
     // OR: libraries: 'places,drawing,visualization'
