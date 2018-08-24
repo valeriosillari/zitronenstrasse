c1385f7657770b9ea3dce42a9f63c36f55dcb791
Valerio Sillari <valerio.sillari@gmail.com>
2018-08-24 11:47:36 +0200

    DEV test deploy develop - 02

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,7 +26,7 @@
     },
     // mounted: WHEN ALL code on server is already loaded!
     mounted () {
-      console.log('🍋 - develop 01')
+      console.log('🍋 - develop 02')
     }
   }
 </script>
