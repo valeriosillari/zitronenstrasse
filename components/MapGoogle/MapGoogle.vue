71eeeecbaac9955329d0e38442b259d826a355c6
Valerio Sillari <valerio.sillari@gmail.com>
2018-08-24 13:14:24 +0200

    [skip ci] added no ssr option. one error is gone, another appears.

--- components/MapGoogle/MapGoogle.vue
+++ components/MapGoogle/MapGoogle.vue
@@ -5,13 +5,15 @@
       START
     </h1>
 
-    <GmapMap
-      :center="{lat:10, lng:10}"
-      :zoom="7"
-      map-type-id="terrain"
-      style="width: 500px; height: 300px"
-    >
-    </GmapMap>
+    <no-ssr>
+      <GmapMap
+        :center="{lat:10, lng:10}"
+        :zoom="7"
+        map-type-id="terrain"
+        style="width: 500px; height: 300px"
+      >
+      </GmapMap>
+    </no-ssr>
 
     <h1>
       END
