fb71a1eeaedb1b9adac919eec07fe85f0cd61ebb
Valerio Sillari <valerio.sillari@gmail.com>
2018-08-27 11:22:36 +0200

    DEV. test logic for window.google 2

--- components/MapGoogle/MapGoogle.vue
+++ components/MapGoogle/MapGoogle.vue
@@ -171,121 +171,124 @@
 
     // mounted: WHEN ALL code on server is already loaded!
     mounted () {
-      // !!! we need this google constant
-      const google = window.google
-
-      // marker custom colors
-      // custom color: a little bit darker then the main one. for the marker looks the same
-      const markerColorFill = '#eab622'
-      const markerColorStroke = '#FFC832'
-      // path inspiration from this codepen:
-      // https://codepen.io/defvayne23/pen/EVYGRw?editors=1010
-      // we follow the svg path to amazon and taken the path from the original url
-      // https://s3-us-west-2.amazonaws.com/s.cdpn.io/134893/pin-red.svg
-      const markerPath = 'M 8 2.1 c 1.1 0 2.2 0.5 3 1.3 c 0.8 0.9 1.3 1.9 1.3 3.1 s -0.5 2.5 -1.3 3.3 l -3 3.1 l -3 -3.1 c -0.8 -0.8 -1.3 -2 -1.3 -3.3 c 0 -1.2 0.4 -2.2 1.3 -3.1 c 0.8 -0.8 1.9 -1.3 3 -1.3 Z'
-      const customMarker = {
-        path: markerPath,
-        fillColor: markerColorFill,
-        strokeColor: markerColorStroke,
-        fillOpacity: 1,
-        scale: 2.5,
-        strokeWeight: 2,
-        // for correct alignmnet of custom SVG icon with map point
-        anchor: new google.maps.Point(11, 12)
-      }
-
-      // follow this tutorial
-      // https://wrightshq.com/playground/placing-multiple-markers-on-a-google-map-using-api-3/
-      const initMap = () => {
-        // init map canvas
-        const map = new google.maps.Map(document.getElementById('google-map'), {
-          center: {
-            lat: 52.48383,
-            lng: 13.4395546
-          },
-          zoom: 14,
-          // map options
-          options: {
-            streetViewControl: false,
-            fullscreenControl: false,
-            mapTypeControl: false,
-            // set custom map styles
-            styles: mapStylesDark
-          }
-        })
-
-        // function for PAN movement
-        google.maps.Map.prototype.panToWithOffset = function (latlng, offsetX, offsetY) {
-          let ov = new google.maps.OverlayView()
-          ov.onAdd = function () {
-            let proj = this.getProjection()
-            let aPoint = proj.fromLatLngToContainerPixel(latlng)
-            aPoint.x = aPoint.x + offsetX
-            aPoint.y = aPoint.y + offsetY
-            map.panTo(proj.fromContainerPixelToLatLng(aPoint))
-          }
-          ov.draw = () => {}
-          ov.setMap(this)
-        }
-
-        const setSingleMarker = (indexNumber, placeID) => {
-          let marker = new google.maps.Marker({
-            position: new google.maps.LatLng(placeID.position.lat, placeID.position.lng),
-            map: map,
-            title: placeID.title,
-            // set icon custom style
-            icon: customMarker
-          })
-
-          // at marker click ...
-          google.maps.event.addListener(marker, 'click', () => {
-            // update info in sidebar with current marker
-            this.currentMarkerDetails.title = placeID.title
-            this.currentMarkerDetails.thumb = placeID.thumb
-            this.currentMarkerDetails.thumbCredits = placeID.thumbCredits
-            this.currentMarkerDetails.address = placeID.address
-            this.currentMarkerDetails.position = placeID.position
-            this.currentMarkerDetails.website = placeID.website
-            this.currentMarkerDetails.fbPage = placeID.fbPage
-
-            // open sidebar + PAN MOVE
-            this.isSidebarOpen(window.innerWidth)
-
-            // todo: set better with NO timeout?
-            // START bounce
-            setTimeout(() => {
-              marker.setAnimation(google.maps.Animation.BOUNCE)
-            }, 400)
-            // END bounce
-            setTimeout(() => {
-              marker.setAnimation(null)
-            }, 1150)
-          })
-
-          return marker
-        // ./ end setSingleMarker
-        }
-
-        // Loop our LIST array and set marker on map
-        for (const [indexNumber, placeID] of placesList.entries()) {
-          setSingleMarker(indexNumber, placeID)
-        }
-
-        // return map so we can used it globally
-        this.map = map
-
-        // TODO: set as function?
-        // check if user is dragging the map. we need it later for close option
-        google.maps.event.addListener(map, 'dragend', () => {
-          this.isMapDragged = true
-        })
-
-        return map
-      // ./ end init
-      }
-
       // wait google Plugin set and attached to window object
-      initMap()
+      if (window.google) {
+        const google = window.google
+
+        // marker custom colors
+        // custom color: a little bit darker then the main one. for the marker looks the same
+        const markerColorFill = '#eab622'
+        const markerColorStroke = '#FFC832'
+        // path inspiration from this codepen:
+        // https://codepen.io/defvayne23/pen/EVYGRw?editors=1010
+        // we follow the svg path to amazon and taken the path from the original url
+        // https://s3-us-west-2.amazonaws.com/s.cdpn.io/134893/pin-red.svg
+        const markerPath = 'M 8 2.1 c 1.1 0 2.2 0.5 3 1.3 c 0.8 0.9 1.3 1.9 1.3 3.1 s -0.5 2.5 -1.3 3.3 l -3 3.1 l -3 -3.1 c -0.8 -0.8 -1.3 -2 -1.3 -3.3 c 0 -1.2 0.4 -2.2 1.3 -3.1 c 0.8 -0.8 1.9 -1.3 3 -1.3 Z'
+        const customMarker = {
+          path: markerPath,
+          fillColor: markerColorFill,
+          strokeColor: markerColorStroke,
+          fillOpacity: 1,
+          scale: 2.5,
+          strokeWeight: 2,
+          // for correct alignmnet of custom SVG icon with map point
+          anchor: new google.maps.Point(11, 12)
+        }
+
+        // follow this tutorial
+        // https://wrightshq.com/playground/placing-multiple-markers-on-a-google-map-using-api-3/
+        const initMap = () => {
+          // init map canvas
+          const map = new google.maps.Map(document.getElementById('google-map'), {
+            center: {
+              lat: 52.48383,
+              lng: 13.4395546
+            },
+            zoom: 14,
+            // map options
+            options: {
+              streetViewControl: false,
+              fullscreenControl: false,
+              mapTypeControl: false,
+              // set custom map styles
+              styles: mapStylesDark
+            }
+          })
+
+          // function for PAN movement
+          google.maps.Map.prototype.panToWithOffset = function (latlng, offsetX, offsetY) {
+            let ov = new google.maps.OverlayView()
+            ov.onAdd = function () {
+              let proj = this.getProjection()
+              let aPoint = proj.fromLatLngToContainerPixel(latlng)
+              aPoint.x = aPoint.x + offsetX
+              aPoint.y = aPoint.y + offsetY
+              map.panTo(proj.fromContainerPixelToLatLng(aPoint))
+            }
+            ov.draw = () => {}
+            ov.setMap(this)
+          }
+
+          const setSingleMarker = (indexNumber, placeID) => {
+            let marker = new google.maps.Marker({
+              position: new google.maps.LatLng(placeID.position.lat, placeID.position.lng),
+              map: map,
+              title: placeID.title,
+              // set icon custom style
+              icon: customMarker
+            })
+
+            // at marker click ...
+            google.maps.event.addListener(marker, 'click', () => {
+              // update info in sidebar with current marker
+              this.currentMarkerDetails.title = placeID.title
+              this.currentMarkerDetails.thumb = placeID.thumb
+              this.currentMarkerDetails.thumbCredits = placeID.thumbCredits
+              this.currentMarkerDetails.address = placeID.address
+              this.currentMarkerDetails.position = placeID.position
+              this.currentMarkerDetails.website = placeID.website
+              this.currentMarkerDetails.fbPage = placeID.fbPage
+
+              // open sidebar + PAN MOVE
+              this.isSidebarOpen(window.innerWidth)
+
+              // todo: set better with NO timeout?
+              // START bounce
+              setTimeout(() => {
+                marker.setAnimation(google.maps.Animation.BOUNCE)
+              }, 400)
+              // END bounce
+              setTimeout(() => {
+                marker.setAnimation(null)
+              }, 1150)
+            })
+
+            return marker
+          // ./ end setSingleMarker
+          }
+
+          // Loop our LIST array and set marker on map
+          for (const [indexNumber, placeID] of placesList.entries()) {
+            setSingleMarker(indexNumber, placeID)
+          }
+
+          // return map so we can used it globally
+          this.map = map
+
+          // TODO: set as function?
+          // check if user is dragging the map. we need it later for close option
+          google.maps.event.addListener(map, 'dragend', () => {
+            this.isMapDragged = true
+          })
+
+          return map
+        // ./ end init
+        }
+
+        initMap()
+      } else {
+        console.log('MAP not loaded 💩 💩 💩')
+      }
 
       // STILL GOT error
 
