0c15bbac5c5607312e3b3db92ec7d1f5b88968bd
Valerio Sillari <valerio.sillari@gmail.com>
2018-08-29 13:33:09 +0200

    [skip ci] trying SVG inline. we got our SVG and no google object set. we have to fix SVG on Vectorial Software now. https://ubilabs.net/en/news/create-google-maps-markers-with-inline-svg-2016-01-08

--- components/MapGoogle/MapGoogle.vue
+++ components/MapGoogle/MapGoogle.vue
@@ -178,11 +178,17 @@
       // https://s3-us-west-2.amazonaws.com/s.cdpn.io/134893/pin-red.svg
       const customMarker = {
         url: `data:image/svg+xml;utf-8, \
-      <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"> \
-        <path fill="red" stroke="white" stroke-width="1.5" d="M3.5 3.5h25v25h-25z" ></path> \
-      </svg>`,
+        <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+        <style type="text/css">
+          .st0{fill:%23EAB622;stroke:%23FFC832;stroke-width:7;stroke-miterlimit:10;}
+        </style>
+        <path class="st0" d="M50,4c9.2,0,18.4,4.2,25.1,10.9c6.7,7.5,10.9,15.9,10.9,26s-4.2,20.9-10.9,27.6L50,94.4l-25.1-26
+        C18.2,61.7,14,51.7,14,40.8c0-10,3.3-18.4,10.9-26C31.6,8.2,40.8,4,50,4z"/>
+        </svg>
+        `
         // we scale it but again with GOOGLE OPTION ... not good for our logic
-        scaledSize: new google.maps.Size(50, 50)
+        // scaledSize: new google.maps.Size(50, 50)
       }
 
       // follow this tutorial
