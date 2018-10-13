d981153dd5f799448c17a209ff77e1ab6caafc23
Valerio Sillari <valerio.sillari@gmail.com>
2018-10-14 01:18:58 +0200

    [skip ci] we need to set pug. set simple test page.

---
+++ components/MapGoogle/MapGoogle.vue
@@ -0,0 +1,18 @@
+<template>
+  <GmapMap
+    :center="{lat:10, lng:10}"
+    :zoom="7"
+    map-type-id="terrain"
+    style="width: 500px; height: 300px"
+  >
+    <GmapMarker
+      v-for="(m, index) in markers"    
+      :key="index"
+      :position="m.position"
+      :clickable="true"
+      :draggable="true"
+      @click="center=m.position"
+    />
+
+  </GmapMap>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,4 @@
-<template>
-  <GmapMap
-    :center="{lat:10, lng:10}"
-    :zoom="7"
-    map-type-id="terrain"
-    style="width: 500px; height: 300px"
-  >
-    <GmapMarker
-      v-for="(m, index) in markers"    
-      :key="index"
-      :position="m.position"
-      :clickable="true"
-      :draggable="true"
-      @click="center=m.position"
-    />
-
-  </GmapMap>
+<template lang='pug'>
+  .test
+    | HEREEE
 </template>
