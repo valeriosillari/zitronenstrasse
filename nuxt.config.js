1d7ef9353ffd1c81c9b1c0dd9affe3d42df6b52a
Valerio Sillari <valerio.sillari@gmail.com>
2018-10-14 01:58:59 +0200

    [skip ci] deactivated linter for working right now.

--- nuxt.config.js
+++ nuxt.config.js
@@ -282,15 +282,15 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+      // // Run ESLint on save
+      // if (ctx.isDev && ctx.isClient) {
+      //   config.module.rules.push({
+      //     enforce: 'pre',
+      //     test: /\.(js|vue)$/,
+      //     loader: 'eslint-loader',
+      //     exclude: /(node_modules)/
+      //   })
+      // }
     },
 
 
