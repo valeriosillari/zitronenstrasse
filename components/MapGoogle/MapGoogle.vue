d4c6f5d5428b86e813823ff2053c164b7ffb0e55
Valerio Sillari <valerio.sillari@gmail.com>
2018-10-14 13:41:39 +0200

    [skip ci] got map with basic options

--- components/MapGoogle/MapGoogle.vue
+++ components/MapGoogle/MapGoogle.vue
@@ -1,7 +1,36 @@
 <template lang="pug">
   GmapMap(
-    :center="{lat:10, lng:10}"
-    :zoom="7"
+    ref='mapRef'
+    :center='center'
+    :zoom='zoom'
+    :options='options'
     style="width: 800px; height: 400px"
   )
 </template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        // map
+        center: {
+          lat: 52.48383,
+          lng: 13.4395546
+        },
+        zoom: 14,
+        // map options
+        options: {
+          streetViewControl: false,
+          fullscreenControl: false,
+          mapTypeControl: false
+        }
+      }
+    },
+
+    // mounted: WHEN ALL code on server is already loaded!
+    mounted () {
+      console.log('HERERREE')
+    }
+  }
+</script>
