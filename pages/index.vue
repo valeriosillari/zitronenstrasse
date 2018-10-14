8484742708f3282f03cf7812347d63a5d21fe5b3
Valerio Sillari <valerio.sillari@gmail.com>
2018-10-14 02:14:30 +0200

    [skip ci] we got all except the map.

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,12 @@
 <template lang="pug">
-  .ciao
-    | YOOO
+  MapGoogle
 </template>
+
+<script>
+  import MapGoogle from '~/components/MapGoogle/MapGoogle.vue'
+  export default {
+    components: {
+      MapGoogle
+    }
+  }
+</script>
