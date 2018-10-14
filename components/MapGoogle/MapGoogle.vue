407b09f556ee619966ac50b48a5f9675503280b4
Valerio Sillari <valerio.sillari@gmail.com>
2018-10-14 02:44:05 +0200

    [skip ci] the basic map logic works. we need to set now my code.

--- components/MapGoogle/MapGoogle.vue
+++ components/MapGoogle/MapGoogle.vue
@@ -1,20 +1,8 @@
-<template lang="pug">
-  .map-test
-    | herererre
+<template>
+  <GmapMap
+    :center="{lat:10, lng:10}"
+    :zoom="7"
+    style="width: 800px; height: 400px"
+  >
+  </GmapMap>
 </template>
-
-<style lang="sass">
-  .map-test
-    background: red
-    color: white
-    min-height: 400px
-</style>
-
-<script>
-  export default {
-    // mounted: WHEN ALL code on server is already loaded!
-    mounted () {
-      console.log('MOUNTED map component')
-    }
-  }
-</script>
