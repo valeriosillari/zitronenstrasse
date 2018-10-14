858a78f12d4ec29c271f24fb87929e22e8d0669a
Valerio Sillari <valerio.sillari@gmail.com>
2018-10-14 13:28:13 +0200

    [skip ci] set map with pug

--- components/MapGoogle/MapGoogle.vue
+++ components/MapGoogle/MapGoogle.vue
@@ -1,8 +1,7 @@
-<template>
-  <GmapMap
+<template lang="pug">
+  GmapMap(
     :center="{lat:10, lng:10}"
     :zoom="7"
     style="width: 800px; height: 400px"
-  >
-  </GmapMap>
+  )
 </template>
