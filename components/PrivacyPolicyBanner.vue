beef34d0a9a1817f7205cc21724e241b210df818
Valerio Sillari <valerio.sillari@gmail.com>
2018-10-14 02:06:11 +0200

    [skip ci] set privacy changes and font changes.

--- components/PrivacyPolicyBanner.vue
+++ components/PrivacyPolicyBanner.vue
@@ -1,16 +1,20 @@
 <template lang="pug">
-  //- small trick for SSR (document not found) using SSR tag.
-  //- info here:
-  //- https://github.com/apertureless/vue-cookie-law/issues/19 | indifisher commented on 23 Apr
-  no-ssr
-    cookie-law(theme='lemon-theme' buttonText='Ok')
-      div(slot='message')
-        | This site uses some cookies in order to count visits. By clicking the 'Ok' button on this banner you give your consent.
-        | More info here about Privacy Policy
-        |
-        nuxt-link.link-text(to='/privacy-policy' title='Privacy Policy Info')
-          | here
-        |.
+  //- you need to wrap the banner in an element.
+  //- the ssr itself not work on nuxt v2
+  .privacy-area
+    //- small trick for SSR (document not found) using SSR tag.
+    //- info here:
+    //- https://github.com/apertureless/vue-cookie-law/issues/19
+    //- indifiser commented on 23 Apr
+    no-ssr
+      cookie-law(theme='lemon-theme' buttonText='Ok')
+        div(slot='message')
+          | This site uses some cookies in order to count visits. By clicking the 'Ok' button on this banner you give your consent.
+          | More info here about Privacy Policy
+          |
+          nuxt-link.link-text(to='/privacy-policy' title='Privacy Policy Info')
+            | here
+          |.
 </template>
 
 
@@ -28,7 +32,6 @@
     background: $color_banner_bg
     .Cookie__content
       color: $color_banner_content_text
-
     .Cookie__button
       padding: 10px 45px
       border: 1px solid $color_banner_btn_border
