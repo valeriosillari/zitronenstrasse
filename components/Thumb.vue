3dc6ad90d4b49637eceedc45ace60835e27ec27d
Valerio Sillari <valerio.sillari@gmail.com>
2019-01-07 00:37:51 +0100

    [skip ci] todo: image come from method. then tehre set option for deploy logic

--- components/Thumb.vue
+++ components/Thumb.vue
@@ -1,9 +1,11 @@
 <template lang="pug">
   .thumb-area
+    //- todo: image concatenated : image need to go from scripts and update at chnage
+    //- the we got methid set correct url based on ENV to deploy
     img.thumb(
       v-if='currentMarkerDetails.thumb'
       :alt='currentMarkerDetails.title'
-      :src="'/thumbs/places/' + currentMarkerDetails.thumb"
+      :src="`/thumbs/places/${currentMarkerDetails.thumb}`"
     )
     //- fallback
     img.thumb(
