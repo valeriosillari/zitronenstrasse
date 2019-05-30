3ee1bc97f8ae53585a3f088a1d9650373d107170
Valerio Sillari <valerio.sillari@gmail.com>
2019-05-30 15:46:48 +0200

    [skip ci] set console log as warning and using prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,8 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // enable console logs (just warnings)
+    'no-console': 1,
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -13,19 +13,19 @@
 </template>
 
 <script>
-  import Navbar from '~/components/Navbar.vue'
-  import FooterArea from '~/components/FooterArea.vue'
-  import NoJsFallback from '~/components/NoJsFallback.vue'
-  import PrivacyPolicyBanner from '~/components/PrivacyPolicyBanner.vue'
-  export default {
-    components: {
-      Navbar,
-      FooterArea,
-      NoJsFallback,
-      PrivacyPolicyBanner
-    },
-    mounted () {
-      console.log('🍋 - 01')
-    }
+import Navbar from '~/components/Navbar.vue'
+import FooterArea from '~/components/FooterArea.vue'
+import NoJsFallback from '~/components/NoJsFallback.vue'
+import PrivacyPolicyBanner from '~/components/PrivacyPolicyBanner.vue'
+export default {
+  components: {
+    Navbar,
+    FooterArea,
+    NoJsFallback,
+    PrivacyPolicyBanner
+  },
+  mounted() {
+    console.log('🍋 - 02')
   }
+}
 </script>
