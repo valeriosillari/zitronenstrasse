2a97c1e53f331499e24b3f58fa0465890bb82137
Valerio Sillari <valerio.sillari@gmail.com>
2019-12-21 22:51:32 +0100

    set one linter ignore file, for a generated file.

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,8 @@ module.exports = {
     'prettier/vue',
   ],
   plugins: ['prettier'],
+  // linter ignore some files here
+  ignorePatterns: ['static/sw.js'],
   // add your custom rules here
   rules: {
     'nuxt/no-cjs-in-config': 'off',
