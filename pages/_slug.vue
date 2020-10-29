76ed66b7b39867d6123bc71e813548331c59b0a9
Valerio Sillari <valerio.sillari@gmail.com>
2020-10-29 22:48:48 +0100

    set basic page?

---
+++ pages/_slug.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    {{ story.name }}
+  </section>
+</template>
+
+<script>
+export default {
+  asyncData(context) {
+    // Check if we are in the editor mode
+    const version =
+      context.query._storyblok || context.isDev ? 'draft' : 'published'
+
+    // Load the JSON from the API
+    return context.app.$storyapi
+      .get(`cdn/stories/${context.params.slug}`, {
+        version,
+      })
+      .then((res) => {
+        return res.data
+      })
+      .catch((res) => {
+        context.error({
+          statusCode: res.response.status,
+          message: res.response.data,
+        })
+      })
+  },
+
+  data() {
+    return { story: { content: {} } }
+  },
+}
+</script>
