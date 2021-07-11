d76024e7d55d8b08b614d86126b9ea0d60e10a50
Valerio Sillari <valerio.sillari@gmail.com>
2021-07-11 23:50:02 +0200

    removed key option. locally works.

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -5,7 +5,7 @@ Vue.use(VueGoogleMaps, {
   load: {
     // our key for API set in google
     // https://nuxtjs.org/api/configuration-env#automatic-injection-of-environment-variables
-    key: process.env.nuxtEnvGoogleMapKey || '',
+    // key: process.env.nuxtEnvGoogleMapKey || '',
     // set version. list of versions here:
     // https://developers.google.com/maps/documentation/javascript/releases
     version: '3.42.7',
