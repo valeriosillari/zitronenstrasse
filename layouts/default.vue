0811bdd98b029cd8e94673fd1c1059ae7d2cf9ac
Valerio Sillari <valerio.sillari@gmail.com>
2021-09-03 23:05:15 +0200

    delete test on layout page

--- layouts/default.vue
+++ layouts/default.vue
@@ -77,13 +77,3 @@ a
 .page-transition-leave-to
   opacity: 0
 </style>
-
-<script>
-export default {
-  mounted() {
-    console.log('========= LAYOUT MOUNTED =========')
-    // console.log(this.process.env.nuxtEnvGoogleMapKey)
-    console.log(this.$config.nuxtEnvGoogleMapKey)
-  },
-}
-</script>
