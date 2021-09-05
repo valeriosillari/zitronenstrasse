ed25547f3e3c1ffb8215e3fa253b4761f13b8e85
Valerio Sillari <valerio.sillari@gmail.com>
2021-09-05 13:50:43 +0200

    try add text on index page in body

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
 <template lang="pug">
-    MapGoogle  
+    MapGoogle
+
+    h1
+        | Zitronenstrasse
+    
+    p
+     | A mapping Project to collect and share romantic spots in Berlin. Little hidden places around the city.
+
 </template>
