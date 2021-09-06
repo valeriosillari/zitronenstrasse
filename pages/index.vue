d4c2cd1ac319a8670dd4039596d0b015a9cf1787
Valerio Sillari <valerio.sillari@gmail.com>
2021-09-06 21:03:52 +0200

    text hidden for index page

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,16 @@
 div(style='height: 100%')
     MapGoogle
 
-    h1
+    //- hidden h1 text for seo
+    h1.text-hidden
         | Zitronenstrasse
 
-    p
+    //- hidden p text for seo
+    p.text-hidden
         | A mapping PROJECT to collect and share romantic spots in Berlin. Bars, cafes, restaurants, bistros, parks and some little hidden places around the city.
 </template>
+
+<style lang="sass">
+.text-hidden
+    display: none
+</style>
