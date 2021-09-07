63d7905d00d319899d42fb2b9f0b90a389efb452
Valerio Sillari <valerio.sillari@gmail.com>
2021-09-07 21:54:38 +0200

    NOW remove all markup on index page, let's see if meta description text alone does the right thing for google result text

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,3 @@
 <template lang="pug">
-div(style='height: 100%')
-    MapGoogle
-
-    //- hidden h1 text for seo
-    h1.text-hidden
-        | Zitronenstrasse
-
-    //- hidden p text for seo
-    p.text-hidden
-        | A mapping PROJECT to collect and share romantic spots in Berlin. Bars, cafes, restaurants, bistros, parks and some little hidden places around the city.
+MapGoogle
 </template>
-
-<style lang="sass">
-.text-hidden
-    display: none
-</style>
