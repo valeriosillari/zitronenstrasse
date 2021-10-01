87ddace584f03899fa2e12fe25f508dd07409e11
Valerio Sillari <valerio.sillari@gmail.com>
2021-10-01 11:40:10 +0200

    some cleanup for starting again

--- components/Overlay.vue
+++ components/Overlay.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.overlay(v-bind:class='{ "is-nav-open": isNavOpen }')
+.overlay(v-bind:class='{ "is-overlay-show": isNavOpen }')
     nav.overlay-menu
         ul.items
             //- css var logi to pass dynamic vars from vue to css
@@ -66,33 +66,14 @@ export default {
 </script>
 
 <style lang="sass">
-@keyframes fadeInRight
-  0%
-    opacity: 0
-    left: 20%
-
-  100%
-    opacity: 1
-    left: 0
 
 .overlay
-  position: fixed
-  // background: $color_navigation_overlay_bg
   background: green
-  top: 0
-  left: 0
-  width: 100%
-  height: 0
-  opacity: 0
-  visibility: hidden
-  transition: opacity $animation_navigation_overlay, visibility $animation_navigation_overlay, height $animation_navigation_overlay
-  overflow: hidden
 
-  nav
+  .overlay-menu
     position: relative
     height: 85%
     top: 50%
-    transform: translateY(-50%)
     font-size: 80px
     text-align: center
 
@@ -111,48 +92,10 @@ export default {
     display: block
     height: calc(100% / var(--navigation-item-number))
     position: relative
-    opacity: 0
     display: flex
     align-items: center
     justify-content: center
 
-  // <a> tag
-  .item-link
-    display: block
-    overflow: hidden
-    color: $color_navigation_overlay_text
-    +underline-from-center()
-    font-family: $font_family_secondary
-    font-size: 6rem
-    line-height: 1.2
-    text-decoration: none
-    +breakpoint($breakpoint_sm)
-      font-size: 11rem
-      line-height: 1
-    &:hover,
-    &:focus
-      color: $color_navigation_overlay_text_hvr
-
-  // // nav is open! set EXTRA BODY class
-  // .is-navigation-open-body
-  //   overflow: hidden
-
-  // nav is open! see overlay
-  &.is-nav-open
-    opacity: 1
-    visibility: visible
-    height: 100%
-
-    .item
-      animation: fadeInRight .5s ease forwards
-      animation-delay: .35s
-
-      &:nth-of-type(2)
-        animation-delay: .4s
-
-      &:nth-of-type(3)
-        animation-delay: .45s
-
-      &:nth-of-type(4)
-        animation-delay: .5s
+  &.is-overlay-show
+    background: #0f0
 </style>
