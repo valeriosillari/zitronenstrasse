af871db692610e4f641b65532f8e11fa0c135028
Valerio Sillari <valerio.sillari@gmail.com>
2021-10-13 22:58:50 +0200

    added todo

--- nuxt.config.js
+++ nuxt.config.js
@@ -155,6 +155,7 @@ export default {
         exclude: [
             // empty
         ],
+        // TODO: set route listening path from storyblok?
         routes: [
             { url: '/', changefreq: 'daily' },
             { url: '/about', changefreq: 'daily' },
