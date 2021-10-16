1f096645c92f9e163d3f443c18b689a4583ccf94
Valerio Sillari <valerio.sillari@gmail.com>
2021-10-16 14:04:11 +0200

    start setting stoyblok string as var from store. we want all string in one place. (II)

--- store/storyblok.js
+++ store/storyblok.js
@@ -1,8 +1,8 @@
-import stringNameForHomePage from '../assets/javascripts/utilities/storyblok.js'
+import data from '../assets/javascripts/utilities/storyblok.js'
 
 export const state = () => ({
     // on storyblok we set a component as "page" to set some trick for our application index page on Nuxt
     // here the storyblock SLUG name, used as "core" for the trick
-    stringNameForHomePage,
-    stringNameForHomePageTemplate: 'page_index',
+    stringNameForHomePage: data.stringNameForHomePage,
+    stringNameForHomePageTemplate: data.stringNameForHomePageTemplate,
 })
