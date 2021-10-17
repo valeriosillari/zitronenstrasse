3a23ec69e496e57f821d0ee63abc6502f8fb1643
Valerio Sillari <valerio.sillari@gmail.com>
2021-10-17 23:38:07 +0200

    set right time (reduced)

--- assets/javascripts/utilities/transitionTime.js
+++ assets/javascripts/utilities/transitionTime.js
@@ -1,6 +1,6 @@
 // on storyblok we set a component as "page" to set some trick for our application index page on Nuxt
 // here the storyblock SLUG name, used as "core" for the trick
 
-const transitionTime = 500
+const transitionTime = 360
 
 export default transitionTime
