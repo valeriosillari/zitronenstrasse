61ad996b822e0036053ff064ecec389271093814
Valerio Sillari <valerio.sillari@gmail.com>
2021-11-18 21:51:57 +0100

    add comment

--- plugins/storyblokComponents.js
+++ plugins/storyblokComponents.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
 import MapGoogle from '~/components/organism/MapGoogle/MapGoogle'
 
+// first value as storyblok block name, the second one as project vue component
 Vue.component('MapGoogle', MapGoogle)
