cfb271b7922f83858ab2a6eee5af71d7ad1ea354
Valerio Sillari <valerio.sillari@gmail.com>
2024-12-01 13:02:35 +0100

    set back logic for single spot fade effect at chnage

--- stores/singleSpotSelected.ts
+++ stores/singleSpotSelected.ts
@@ -1,6 +1,22 @@
 import type { TypeSingleSpot } from '../types/TypeSingleSpot'
 import GQL_QUERY_SINGLE_SPOT_BY_ID from '../graphql/singleSpot'
 
+const spotInitialState = {
+    currentSpotData: {
+        id: 0,
+        title: '',
+        imageCredits: '',
+        image: {
+            title: '',
+            url: '',
+        },
+        addressStreet: '',
+        urlWebsite: '',
+        urlFacebook: '',
+        urlInstagram: '',
+    },
+}
+
 const apiCallResponse = async (singleSpotSysId: string) => {
     const { data } = await useAsyncQuery<TypeSingleSpot>(
         GQL_QUERY_SINGLE_SPOT_BY_ID,
@@ -16,27 +32,18 @@ export const useSingleSpotSelectedStore = defineStore(
     'singleSpotSelectedStore',
     {
         // arrow function recommended for full type inference
-        state: () => ({
-            currentSpotData: {
-                id: 0,
-                title: '',
-                imageCredits: '',
-                image: {
-                    title: '',
-                    url: '',
-                },
-                addressStreet: '',
-                urlWebsite: '',
-                urlFacebook: '',
-                urlInstagram: '',
-            },
-        }),
+        state: () => spotInitialState,
 
         actions: {
             updateSingleSpotSelectedState(singleSpotSysId: string) {
-                apiCallResponse(singleSpotSysId).then((singleSpotData) => {
-                    this.currentSpotData = singleSpotData
-                })
+                // reset | enable UI "fade" effect
+                this.currentSpotData = spotInitialState
+
+                this.currentSpotData = apiCallResponse(singleSpotSysId).then(
+                    (singleSpotData) => {
+                        this.currentSpotData = singleSpotData
+                    }
+                )
             },
         },
     }
