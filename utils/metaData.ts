4a670e1bedc1edb5474a3df3804fe462d4cc5711
Valerio Sillari <valerio.sillari@gmail.com>
2025-03-15 13:00:52 +0100

    set hid property again

--- utils/metaData.ts
+++ utils/metaData.ts
@@ -20,16 +20,19 @@ const metaData = [
     },
 
     {
+        hid: 'description',
         name: 'description',
         content: ogDescription,
     },
 
     {
+        hid: 'author',
         name: 'author',
         content: authorName,
     },
 
     {
+        hid: 'keywords',
         name: 'keywords',
         content: CONSTANT.keywords,
     },
@@ -37,46 +40,55 @@ const metaData = [
     // ======================
     // OG options for open graph: Fb and Linkedin
     {
+        hid: 'og:title',
         property: 'og:title',
         content: ogTitle,
     },
 
     {
+        hid: 'og:description',
         property: 'og:description',
         content: ogDescription,
     },
 
     {
+        hid: 'og:type',
         property: 'og:type',
         content: ogType,
     },
 
     {
+        hid: 'og:url',
         property: 'og:url',
         content: ogUrl,
     },
 
     {
+        hid: 'og:image',
         property: 'og:image',
         content: ogImage,
     },
 
     {
+        hid: 'og:image:width',
         property: 'og:image:width',
         content: ogImageWidth,
     },
 
     {
+        hid: 'og:image:height',
         property: 'og:image:height',
         content: ogImageHeight,
     },
 
     {
+        hid: 'og:image:type',
         property: 'og:image:type',
         content: 'image/png',
     },
 
     {
+        hid: 'og:image:alt',
         property: 'og:image:alt',
         content: ogTitle,
     },
@@ -84,36 +96,43 @@ const metaData = [
     // ======================
     // Twitter card
     {
+        hid: 'twitter:card',
         name: 'twitter:card',
         content: 'summary_large_image',
     },
 
     {
+        hid: 'twitter:site',
         name: 'twitter:site',
         content: '@stellavalerio',
     },
 
     {
+        hid: 'twitter:creator',
         name: 'twitter:creator',
         content: authorName,
     },
 
     {
+        hid: 'twitter:title',
         name: 'twitter:title',
         content: ogTitle,
     },
 
     {
+        hid: 'twitter:description',
         name: 'twitter:description',
         content: ogDescription,
     },
 
     {
+        hid: 'twitter:image',
         name: 'twitter:image',
         content: ogImage,
     },
 
     {
+        hid: 'twitter:image:alt',
         name: 'twitter:image:alt',
         content: ogTitle,
     },
