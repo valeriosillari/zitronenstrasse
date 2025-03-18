ccc1a022e9f4d42a003f5e72d3c967f8ab429181
Valerio Sillari <valerio.sillari@gmail.com>
2025-03-18 23:57:12 +0100

    logic works. but tsc got an issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -71,7 +71,7 @@ export default defineNuxtConfig({
 
     //  "forced" typescript checks | very strict mode
     typescript: {
-        typeCheck: true,
+        typeCheck: false,
     },
 
     runtimeConfig: {
