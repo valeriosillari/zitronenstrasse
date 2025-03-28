7600377a7554fe2dbc91fc680082933e36168d8b
Valerio Sillari <valerio.sillari@gmail.com>
2025-03-28 23:10:54 +0100

    got soemthing

--- components/atoms/rich-text.vue
+++ components/atoms/rich-text.vue
@@ -1,15 +1,40 @@
 <template>
-    <div class="b-rich-text" v-html="richText" />
+    <div
+        v-if="sanitizedRichText"
+        class="b-rich-text"
+        v-html="sanitizedRichText"
+    />
 </template>
 
 <script lang="ts" setup>
+// import { JSDOM } from 'jsdom'
+// import DOMPurify from 'dompurify'
+
 const props = defineProps({
     richText: {
         type: String,
         required: true,
     },
 })
-const { richText } = props
+
+const sanitizedRichText = ref<string>('')
+
+// sanitize DOM html received, to prevent malicious code and injections
+// const sanitizedRichText = computed(() => DOMPurify.sanitize(richText))
+
+if (import.meta.server) {
+    // On the server, we use jsdom to create a virtual window.
+    const { JSDOM } = await import('jsdom')
+    const jsdomWindow = new JSDOM('').window
+    // Import DOMPurify and initialize it with the virtual window.
+    const createDOMPurify = (await import('dompurify')).default
+    const DOMPurify = createDOMPurify(jsdomWindow)
+    sanitizedRichText.value = DOMPurify.sanitize(props.richText)
+} else {
+    // On the client, import DOMPurify normally.
+    const DOMPurify = (await import('dompurify')).default
+    sanitizedRichText.value = DOMPurify.sanitize(props.richText)
+}
 </script>
 
 <style lang="sass">
