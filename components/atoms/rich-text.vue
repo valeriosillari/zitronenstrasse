dbeef8705aae1ce80082d5607e81617832046c0b
Valerio Sillari <valerio.sillari@gmail.com>
2025-03-29 00:03:22 +0100

    removed eslint warning for v-html, update css indent

--- components/atoms/rich-text.vue
+++ components/atoms/rich-text.vue
@@ -14,14 +14,14 @@ const { richText } = props
 
 <style lang="sass">
 .b-rich-text
-  b
-    color: $color_text_accent
-    font-weight: 400
+    b
+        color: $color_text_accent
+        font-weight: 400
 
-  h2
-    margin-top: $space_04
+    h2
+        margin-top: $space_04
 
-  h3
-    @include font-size($h4-font-size)
-    margin-top: $space_03
+    h3
+        @include font-size($h4-font-size)
+        margin-top: $space_03
 </style>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,9 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt()
+export default withNuxt({
+    rules: {
+        // i trust data coming fro richtext by API
+        'vue/no-v-html': 'off',
+    },
+})
