3fd7a3e14bc2b95005a7a31407051aa859df75e9
Valerio Sillari <valerio.sillari@gmail.com>
2025-04-01 23:15:49 +0200

    removed cleanup

--- stores/singleSpotSelected.ts
+++ stores/singleSpotSelected.ts
@@ -29,11 +29,9 @@ export const useSingleSpotSelectedStore = defineStore(
             },
 
             updateSingleSpotSelectedState(singleSpotSysId: string) {
-                console.log('>>> API call START ...')
                 apiCallResponse(singleSpotSysId).then((singleSpotData) => {
                     this.currentSpotData = singleSpotData
                     this.isSpotShown = true
-                    console.log('>>> API call DONE')
                 })
             },
         },
