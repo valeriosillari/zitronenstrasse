1e641c775dc6a1ee963a20e6ead750a7d936028d
Valerio Sillari <valerio.sillari@gmail.com>
2025-06-14 00:17:41 +0200

    fix types and linter

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
     e2e: {
         baseUrl: 'http://localhost:8000', // Nuxt default dev server
-        setupNodeEvents(on, config) {
+        setupNodeEvents() {
             // implement node event listeners here if needed
         },
         specPattern: 'cypress/e2e/**/*.cy.{js,ts}', // pattern for test files
